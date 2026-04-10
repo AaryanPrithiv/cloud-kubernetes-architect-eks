052c31c300546791a0b705660fcfc1fca70a12bf
Prithivkumar M <prithivkmrgmail.com>
2026-04-10 22:22:39 +0530

    Infra-provisoning

---
+++ aws-devops/EKS/terraform/kubeconfig.tf
@@ -0,0 +1,18 @@
+#EKS/terraform/kubeconfig.tf
+
+data "template_file" "kubeconfig" {
+  template = file("${path.module}/templates/kubeconfig.yml.j2")
+
+  vars = {
+    eks_name     = module.eks.cluster_name
+    eks_endpoint = module.eks.cluster_endpoint
+    eks_ca       = module.eks.cluster_certificate_authority_data
+    eks_region   = var.eks_region       # <- make sure this exists
+  }
+}
+
+# Write kubeconfig to ~/.kube/config
+resource "local_file" "kubeconfig" {
+  content  = data.template_file.kubeconfig.rendered
+  filename = "${pathexpand("~/.kube/config")}"
+}
